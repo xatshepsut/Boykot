ecf30f668cccf0f9acfeecca8586e4ff00376392
Aidela Karamyan <akaramyan@macadamian.com>
2014-05-21 16:20:45 +0400

    Added PCL with product view model and json parsing logic.

---
+++ Boykot.PCL/Common/NotifyPropertyChangedBase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace Boykot.PCL.Common
+{
+    public class NotifyPropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void RaisePropertyChanged<T>(Expression<Func<T>> propertyExpression)
+        {
+            var propertyName = ExtractPropertyName(propertyExpression);
+            RaisePropertyChanged(propertyName);
+        }
+
+        protected string ExtractPropertyName<T>(Expression<Func<T>> propertyExpression)
+        {
+            if (propertyExpression == null)
+            {
+                throw new ArgumentNullException("propertyExpression");
+            }
+
+            var memberExpression = propertyExpression.Body as MemberExpression;
+            if (memberExpression == null)
+            {
+                throw new ArgumentException("The expression is not a member access expression.", "propertyExpression");
+            }
+
+            var property = memberExpression.Member as PropertyInfo;
+            if (property == null)
+            {
+                throw new ArgumentException("The member access expression does not access a property.", "propertyExpression");
+            }
+
+            if (!property.DeclaringType.GetTypeInfo().IsAssignableFrom(this.GetType().GetTypeInfo()))
+            {
+                throw new ArgumentException("The referenced property belongs to a different type.", "propertyExpression");
+            }
+
+            var getMethod = property.GetMethod;
+            if (getMethod == null)
+            {
+                // this shouldn't happen - the expression would reject the property before reaching this far
+                throw new ArgumentException("The referenced property does not have a get method.", "propertyExpression");
+            }
+
+            if (getMethod.IsStatic)
+            {
+                throw new ArgumentException("The referenced property is a static property.", "propertyExpression");
+            }
+
+            return memberExpression.Member.Name;
+        }
+    }
+}
---
+++ Boykot.PCL/Models/Bootstrap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boykot.PCL.Models.Bootstrap
+{
+    public class Category
+    {
+        public string categoryId { get; set; }
+        public string categoryName { get; set;}
+    }
+
+    public class Product
+    {
+        public string itemName { get; set; }
+        public string imageHash { get; set; }
+        public string imageUrl { get; set; }
+        public Category category { get; set; }
+        public List<String> itemBarCodes { get; set; }
+    }
+
+    public class BootstrapInfo
+    {
+        public List<Category> categoryList { get; set; }
+        public List<Product> productList { get; set; }
+    }
+}
+
