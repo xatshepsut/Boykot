c5483841c009038c04e362bb6bbffce02ff96d00
Aidela Karamyan <akaramyan@macadamian.com>
2014-05-23 13:48:18 +0400

    Added PCL and renamed solution  * Added PCL with new product model and web request, json parsing logic  * Renamed solution to Boykot

---
+++ Boykot.PCL/Helpers/JSONParser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boykot.PCL.Helpers
+{
+    public class JSONParser<T> where T : class, new()
+    {
+        public T parse(string data)
+        {
+            T obj = new T();
+            var ms = new MemoryStream(Encoding.Unicode.GetBytes(data));
+
+            try
+            {
+                System.Runtime.Serialization.Json.DataContractJsonSerializer serializer = new System.Runtime.Serialization.Json.DataContractJsonSerializer(obj.GetType());
+                obj = serializer.ReadObject(ms) as T;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("JSON Parser exception: " + ex.Message);
+            }
+
+            return obj;
+        }
+    }
+}
---
+++ Boykot.PCL/Models/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boykot.PCL.Models
+{
+    public class Product
+    {
+        #region Public Properties
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string Category { get; set; }
+        public List<String> ItemBarCodes { get; set; }
+        #endregion
+
+        #region Internal Properties
+        protected Models.Bootstrap.Product Bootstrap { get; set; }
+        #endregion
+
+        public Product()
+        {
+        }
+
+        static public Product FromBootstrap(Models.Bootstrap.Product product)
+        {
+            return new Product
+            {
+                Name = product.itemName,
+                ImageUrl = product.imageUrl,
+                Category = product.category.categoryName,
+                ItemBarCodes = product.itemBarCodes,
+                Bootstrap = product
+            };
+        }
+    }
+}
